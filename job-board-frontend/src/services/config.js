df505303ccde87de552a4e8e22039a45dbf5174b
waildevil <wail.homan1@hotmail.com>
2025-09-09 16:18:01 +0200

    url fix

--- job-board-frontend/src/services/config.js
+++ job-board-frontend/src/services/config.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+export const API_URL = process.env.REACT_APP_API_URL;
 export const BACKEND_URL = process.env.REACT_APP_API_URL;
 console.log("👉 API_URL is:", API_URL);
 
